8fc9aeeef9159ae4995d263edd41093dd3f53dca
stevestriker <stever185@gmail.com>
2023-10-19 12:12:30 +0200

    Gsap Fixed for City

--- Script.js
+++ Script.js
@@ -1,11 +1,22 @@
-let timeline = gsap.timeline();
+let timeline = gsap.timeline({ defaults: { duration: .25, ease: "power2.out"} });
 
-timeline.from('#Sea', {opacity: 0, duration:2})
-        .from('#B1', {duration:.15, yPercent:100, ease:'spring'}, .25)
-        .from('#B2', {duration:.15, yPercent:100, ease:'spring'})
-        .from('#B3', {duration:.15, yPercent:100, ease:'spring'})
-        .from('#B4', {duration:.15, yPercent:100, ease:'spring'})
-        .from('#B5', {duration:.15, yPercent:100, ease:'spring'})
-        .from('#B6', {duration:.15, yPercent:100, ease:'spring'})
-        .from('#Sky', {duration:.27, yPercent:-100, ease:'back'})
+timeline.from("#B1", {yPercent:100})
+        .from('#B2', {yPercent:100})
+        .from('#B3', {yPercent:100})
+        .from('#B4', {yPercent:100})
+        .from('#B5', {yPercent:-100})
+        .from('#B6', {yPercent:-100})
+        .from('#Sky', {yPercent:-100, duration:.45, ease:'bounce'})
+        .from('#Sea', {opacity: 0, duration:6}, .15, "seaFadeIn")
+
+
+
+// timeline.from('#Sea', {opacity: 0, duration:6, yPercent:0}, .15, "seaFadeIn")
+//         .from('#B1', {duration:.35, yPercent:100, ease:'bounce'}, .3)
+//         .from('#B2', {duration:.35, yPercent:100, ease:'spring'}, .4)
+//         .from('#B3', {duration:.35, yPercent:100, ease:'spring'}, .5)
+//         .from('#B4', {duration:.35, yPercent:100, ease:'bounce'}, .6)
+//         .from('#B5', {duration: 1, yPercent:-100, ease:'back'}, .7)
+//         .from('#B6', {duration: 1, yPercent:-100, ease:'back'}, 1)
+//         .from('#Sky', {duration: .65, yPercent:-100, ease: "power4.out"}, 1.3)
         
